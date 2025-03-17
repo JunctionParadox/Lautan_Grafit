09739d59870db51645440a2365e618bbcf7f77bf
JunctionParadox <Novasigma89@gmail.com>
2025-03-17 22:36:33 +0100

    fixed drawing issues, moved events and changed page layout

--- script/main.js
+++ script/main.js
@@ -3,6 +3,15 @@ const ctx = canvas.getContext("2d");
 let isDrawing = false;
 let x = 0;
 let y = 0;
+	
+canvas.onmousemove = function() {moveCursor(event)};
+canvas.onmousedown = function() {cursorActivate(event)};
+canvas.onmouseup = function() {cursorDeactivate(event)};
+canvas.onmousleave = function() {cursorExit()};
+
+//MUST BE ROUND BECAUSE THE DEFAULT VALUE CAUSES JAGGED LINES AND DOESN'T COMPREHEND 1-DIMENSIONAL LINES
+ctx.lineCap = "round";
+ctx.imageSmoothingEnabled = false;
 
 function moveCursor(e) {
 	if (isDrawing) {
@@ -12,7 +21,7 @@ function moveCursor(e) {
 	}
 };
 
-function cursorActive(e) {
+function cursorActivate(e) {
 	x = e.offsetX;
 	y = e.offsetY;
 	isDrawing = true;
@@ -27,10 +36,16 @@ function cursorDeactivate(e) {
 	}
 };
 
+function cursorExit() {
+	if (isDrawing) {
+		isDrawing = false;
+	}
+};
+
 function drawDefault(ctx, x1, y1, x2, y2) {
 	ctx.beginPath();
-	ctx.strokeStyle = "#000000"
-	ctx.lineWidth = 1;
+	ctx.strokeStyle = "#000000";
+	ctx.lineWidth = 10;
 	ctx.moveTo(x1, y1);
 	ctx.lineTo(x2, y2);
 	ctx.stroke();
