8109123265ef120b0acc1af11d7ee9c15b99fc04
Delano <92526313+JunctionParadox@users.noreply.github.com>
2025-04-02 01:07:49 +0200

    Line_draw_1_initial
    * Added line drawing functionality and basic testing

--- script/main.js
+++ script/main.js
@@ -1,13 +1,19 @@
 const canvas = document.getElementById("defaultCanvas");
+const pencil = document.getElementById("pencilTool");
+const line = document.getElementById("lineTool");
 const ctx = canvas.getContext("2d");
+var state = 0;
+var canvasBackgroundColor =  "#FFFFFF";
 let isDrawing = false;
 let x = 0;
 let y = 0;
-	
+
+pencil.onclick = function() {togglePencil()};
+line.onclick = function() {toggleLine()};
 canvas.onmousemove = function() {moveCursor(event)};
 canvas.onmousedown = function() {cursorActivate(event)};
 canvas.onmouseup = function() {cursorDeactivate(event)};
-canvas.onmousleave = function() {cursorExit()};
+canvas.onmouseleave = function() {cursorExit()};
 
 //MUST BE ROUND BECAUSE THE DEFAULT VALUE CAUSES JAGGED LINES AND DOESN'T COMPREHEND 1-DIMENSIONAL LINES
 ctx.lineCap = "round";
@@ -15,9 +21,11 @@ ctx.imageSmoothingEnabled = false;
 
 function moveCursor(e) {
 	if (isDrawing) {
-		drawDefault(ctx, x, y, e.offsetX, e.offsetY);
-		x = e.offsetX;
-		y = e.offsetY;
+		if (state == 0) {
+			drawDefault(ctx, x, y, e.offsetX, e.offsetY);
+			x = e.offsetX;
+			y = e.offsetY;
+		}
 	}
 };
 
@@ -29,7 +37,13 @@ function cursorActivate(e) {
 
 function cursorDeactivate(e) {
 	if (isDrawing) {
-		drawDefault(ctx, x, y, e.offsetX, e.offsetY);
+		if(state == 0) {
+			drawDefault(ctx, x, y, e.offsetX, e.offsetY);
+		}
+		else if(state == 1) {
+			console.log(ctx, x, y, e.offsetX, e.offsetY);
+			drawLine(ctx, x, y, e.offsetX, e.offsetY);
+		}
 		x = e.offsetX;
 		y = e.offsetY;
 		isDrawing = false;
@@ -50,4 +64,28 @@ function drawDefault(ctx, x1, y1, x2, y2) {
 	ctx.lineTo(x2, y2);
 	ctx.stroke();
 	ctx.closePath();
+};
+
+function drawLine(ctx, x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.strokeStyle = "#000000";
+	ctx.lineWidth = 10;
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	if (x1 != x2 && y1 != y2) {
+		ctx.stroke();
+	}
+	ctx.closePath();
+};
+	
+
+function togglePencil() {
+	state = 0;
+	console.log(state);
+};
+
+function toggleLine() {
+	state = 1;
+	console.log(state);
+=======
 };
