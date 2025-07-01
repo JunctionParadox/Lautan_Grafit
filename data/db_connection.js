cea658ae94b6dd25f18998064b3336d6f1680a97
JunctionParadox <Novasigma89@gmail.com>
2025-07-02 00:49:28 +0200

    Added env setting to disable database connection

--- data/db_connection.js
+++ data/db_connection.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2');
 
-module.exports  = mysql.createConnection({
-	host: "localhost",
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME
-});
+if(process.env.DEV_MODE === 'true') {
+	require('dotenv').config();
+	module.exports  = mysql.createConnection({
+		host: "localhost",
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME
+	});
 
 console.log(process.env.DB_USER)
 console.log(process.env.DB_PASS)
+}
