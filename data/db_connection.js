31e492a94687b1dbeb951523717f4aec126f45be
JunctionParadox <Novasigma89@gmail.com>
2025-07-02 00:51:09 +0200

    Minor database connection change

--- data/db_connection.js
+++ data/db_connection.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 
 if(process.env.DEV_MODE === 'true') {
-	require('dotenv').config();
 	module.exports  = mysql.createConnection({
 		host: "localhost",
 		user: process.env.DB_USER,
